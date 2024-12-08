4f342f8b9f85e71cc8909544583ea0d3f11ad465
Tyler Wu <tylerwu2222@gmail.com>
2024-12-08 12:12:33 -0800

    add youzi and ecology pieces to prisma

---
+++ src/app/posts/project/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+// components
+import PostHeader from '../../PostHeader';
+import Scrollspy from '@/app/components/Scrollspy/Scrollspy';
+
+// helpers
+import { getPostBySlug } from '@/lib/prisma/prisma';
+
+// types
+import { Post } from '@/types/extendedPrismaTypes';
+
+export default async function page({
+    params,
+}: {
+    // params: Promise<{ slug: string }>
+    params: { slug: string }
+}) {
+    const slug = (await params).slug;
+
+    // query the mdx from prisma based on the slug
+    const postMetaData = await getPostBySlug(slug) as Post;
+
+    // console.log('post metadata', postMetaData);
+
+    // get the markdown dynamically based on the slug
+    const { default: PostMarkdown, metadata } = await import(
+        `@/app/posts/project/${slug}/${slug}.mdx`
+    );
+    // const PostMarkdown = dynamic(() => import(`@/app/posts/writing/${slug}/${slug}.mdx`))
+
+    return (
+        <div className='py-5'>
+            {/* header content (tn, caption, date, author) */}
+            <PostHeader post={postMetaData} />
+            {/* post content (queried via mdx) */}
+            <article className='prose max-w-none'>
+                <PostMarkdown />
+            </article>
+            {/* scrollspy if post has scrollspy */}
+            {postMetaData.hasScrollspy ?
+                <Scrollspy />
+                : <></>
+            }
+        </div>
+    )
+}
--- src/scripts/site_automation/createNewPost.mjs
+++ src/scripts/site_automation/createNewPost.mjs
@@ -4,6 +4,8 @@
 // npm run create-post -- --section="writing" --postName="Title: type it out naturally!" --tags="tag1,tag2" --caption="this"
 // DATA
 // npm run create-post -- --postName="data piece" --tags="tag1,tag2" --caption="this"
+// PROJECT
+// npm run create-post -- --section="project" --postName="" --postDate="2023-05-10" --tags="dev,full-stack,"
 
 // check createNewArticle() fn for params, paramName is : process.env.npm_config_paramName
 
@@ -11,6 +13,7 @@
 import {
   sanitizeFileName,
   getDashedString,
+  quoteCommaSepString
   // snakeToCamel,
 } from "../../lib/stringFormatting.ts";
 
@@ -20,7 +23,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // prisma
-import prisma from "@/db.js";
 import {
   createAuthorsPostConnection,
   createManyTags,
@@ -32,33 +34,38 @@ import {
 } from "../../lib/prisma/prisma.js";
 
 // create folder to hold content of new post
-function createPostFolder(section_name, post_name, worktime_days) {
-  // console.log('section name', section_name);
-  // Get today's date
-  const today = new Date();
+function createPostFolder(section_name, post_name, post_date, worktime_days) {
+  console.log('creating post folder...')
+  // if post date given, use that
+  let postDate;
+  if (post_date) {
+    postDate = post_date;
+  }
+  // otherwise calculate using worktime
+  else {
+    // Get today's date
+    const today = new Date();
 
-  // Calculate the date [worktime_days] from today
-  const estimatedPublicationDate = new Date(
-    today.getTime() + worktime_days * 24 * 60 * 60 * 1000,
-  );
-  const estimatedPublicationDateYear = estimatedPublicationDate.getFullYear();
-  const estimatedPublicationDateMonthNoZero = String(
-    estimatedPublicationDate.getMonth() + 1,
-  );
-  const estimatedPublicationDateMonth =
-    estimatedPublicationDateMonthNoZero.padStart(2, "0");
-  const estimatedPublicationDateDayNoZero = String(
-    estimatedPublicationDate.getDate(),
-  );
-  const estimatedPublicationDateDay =
-    estimatedPublicationDateDayNoZero.padStart(2, "0");
+    // Calculate the date [worktime_days] from today
+    const estimatedPublicationDate = new Date(
+      today.getTime() + worktime_days * 24 * 60 * 60 * 1000,
+    );
+    const estimatedPublicationDateYear = estimatedPublicationDate.getFullYear();
+    const estimatedPublicationDateMonthNoZero = String(
+      estimatedPublicationDate.getMonth() + 1,
+    );
+    const estimatedPublicationDateMonth =
+      estimatedPublicationDateMonthNoZero.padStart(2, "0");
+    const estimatedPublicationDateDayNoZero = String(
+      estimatedPublicationDate.getDate(),
+    );
+    const estimatedPublicationDateDay =
+      estimatedPublicationDateDayNoZero.padStart(2, "0");
 
-  // Construct the folder/slug name: yyyy-mm-dd-dashed-post-name
-  const postDate = `${estimatedPublicationDateYear}-${estimatedPublicationDateMonth}-${estimatedPublicationDateDay}`;
+    // Construct the folder/slug name: yyyy-mm-dd-dashed-post-name
+    postDate = `${estimatedPublicationDateYear}-${estimatedPublicationDateMonth}-${estimatedPublicationDateDay}`;
+  }
   const slug = postDate + `-${getDashedString(post_name)}`;
-
-  // RETIRED, use dash date everywhere slash date for mdx metadata
-  // const slashDate = `${estimatedPublicationDateMonthNoZero}/${estimatedPublicationDateDayNoZero}/${estimatedPublicationDateYear}`
 
   // Create the folder
   // relative path 1: scripts, 2: src --> app -> posts
@@ -77,6 +84,8 @@ function createPostFolder(section_name, post_name, worktime_days) {
 
 // adds files to folder
 function addContent(files, folders, folderPath) {
+  console.log('adding files to folder...')
+  console.log('files',files);
   // create files and write content
   files.forEach((file) => {
     const filePath = path.join(folderPath, file.name);
@@ -160,10 +169,11 @@ function createPostFiles(
   visible,
   post_folder_path,
 ) {
+  console.log('adding post files...')
   // console.log('tags in CPF', typeof tags, tags);
+
   // defined content based on section
   let files, folders;
-
   // DATA
   if (section == "data") {
     if (template_type == "mdx") {
@@ -173,8 +183,8 @@ function createPostFiles(
           content: `export const metadata = {
     publishDate: "${dash_date}",
     title: "${post_name}",
-    authors: "[${authors}]"
-    tags:  "[${tags}]"
+    authors: [${quoteCommaSepString(authors)}]
+    tags:  [${quoteCommaSepString(tags)}]
     readingTime: "${reading_time}",
     caption: "${caption}",
     templateType: "${template_type}",
@@ -192,8 +202,8 @@ function createPostFiles(
           content: `export const metadata = {
     publishDate: "${dash_date}",
     title: "${post_name}",
-    authors: "[${authors}]",
-    tags:  "[${tags}]",
+    authors: [${quoteCommaSepString(authors)}],
+    tags:  [${quoteCommaSepString(tags)}],
     readingTime: "${reading_time}",
     caption: "${caption}",
     templateType: "${template_type}",
@@ -234,8 +244,8 @@ export default Post;`,
         content: `export const metadata = {
     publishDate: "${dash_date}",
     title: "${post_name}",
-    authors: "[${authors}]",
-    tags:  "[${tags}]",
+    authors: [${quoteCommaSepString(authors)}],
+    tags:  [${quoteCommaSepString(tags)}],
     readingTime: "${reading_time}",
     caption: "${caption}",
     hasScrollspy: "${scrollspy}",
@@ -257,8 +267,8 @@ export default Post;`,
         content: `export const metadata = {
     publishDate: "${dash_date}",
     title: "${post_name}",
-    authors: "[${authors}]",
-    tags:  "[${tags}]",
+    authors: [${quoteCommaSepString(authors)}],
+    tags:  [${quoteCommaSepString(tags)}],
     visibility: "${visible}"
 }
 
@@ -277,8 +287,8 @@ export default Post;`,
         content: `export const metadata = {
     publishDate: "${dash_date}",
     title: "${post_name}",
-    authors: "[${authors}]",
-    tags:  "[${tags}]",
+    authors: [${quoteCommaSepString(authors)}],
+    tags:  [${quoteCommaSepString(tags)}],
     visibility: "${visible}"
 }
 
@@ -288,15 +298,15 @@ export default Post;`,
     folders = [];
   }
   // PROJECT
-  else if (section == "projects") {
+  else if (section == "project") {
     files = [
       {
         name: `${getDashedString(slug)}.mdx`,
         content: `export const metadata = {
     publishDate: "${dash_date}"
     title: "${post_name}"
-    authors: "[${authors}]"
-    tags:  "[${tags}]"
+    authors: [${quoteCommaSepString(authors)}]
+    tags:  [${quoteCommaSepString(tags)}]
     readingTime: "${reading_time}"
     caption: "${caption}"
     hasScrollspy: "${scrollspy}"
@@ -349,10 +359,11 @@ const createNewPost = () => {
   // get params from
   const section = process.env.npm_config_section || "data";
   const post_name = process.env.npm_config_postName || "new_post";
+  const post_date = process.env.npm_config_postDate || undefined; //yyyy-mm-dd
   const work_time = process.env.npm_config_workTime || 14;
-  let authors = process.env.npm_config_authors || ["Tyler Wu"];
+  let authors = process.env.npm_config_authors || "Tyler Wu";
   const template_type = process.env.npm_config_templateType || "mdx";
-  let tags = process.env.npm_config_tags || ["tag"];
+  let tags = process.env.npm_config_tags || "tag";
   const caption = process.env.npm_config_caption || "caption";
   const reading_time = process.env.npm_config_readingTime || "8 min";
   const scrollspy = process.env.npm_config_scrollspy || true;
@@ -363,6 +374,7 @@ const createNewPost = () => {
   const [slug, post_folder_path, dash_date] = createPostFolder(
     section,
     sanitizedFileName,
+    post_date,
     work_time,
   );
 
