4ec916d0a761e6a9ff028dbc89ac9bfa310b3cc1
Tyler Wu <tylerwu2222@gmail.com>
2024-12-17 13:46:18 -0800

    add badges for collaborator thumbnails

--- src/app/api/authors/route.ts
+++ src/app/api/authors/route.ts
@@ -31,15 +31,16 @@ export async function GET() {
   // console.log('BE author before flatten', allAuthors);
 
   // get total views and oinks on posts
-  let countedAuthors = allAuthors.map((author) => ({
+  const countedAuthors = allAuthors.map((author) => ({
     ...author,
-    viewCount: author.AuthorsOnPosts.reduce((acc,p) => acc + p.Post.views, 0),
-    oinkCount: author.AuthorsOnPosts.reduce((acc,p) => acc + p.Post.oinks, 0),
-    postCount: author.AuthorsOnPosts.filter((p) => p.Post.visibility === "visible").length // only get published posts
-  }
-  ));
+    viewCount: author.AuthorsOnPosts.reduce((acc, p) => acc + p.Post.views, 0),
+    oinkCount: author.AuthorsOnPosts.reduce((acc, p) => acc + p.Post.oinks, 0),
+    postCount: author.AuthorsOnPosts.filter(
+      (p) => p.Post.visibility === "visible",
+    ).length, // only get published posts
+  }));
 
-  let formattedAllAuthors = flattenJoinData(countedAuthors, {
+  const formattedAllAuthors = flattenJoinData(countedAuthors, {
     AuthorsOnPosts: "posts", // post ids
     AuthorsOnTags: "tags", // tag names
   });
