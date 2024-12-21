7d87637e7752fc0c1141cd0f2aded9f09a0af118
Tyler Wu <tylerwu2222@gmail.com>
2024-12-21 08:21:03 -0800

    test wip dev/post visibility

---
+++ src/app/components/markdown/hovertext/DefinitionText.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { PropsWithChildren } from 'react';
+
+interface DefinitionText extends PropsWithChildren{
+    
+}
+
+const DefinitionText = ({children}:DefinitionText) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default DefinitionText;
